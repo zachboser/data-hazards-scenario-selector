f0e7a66e783716e60749b2ef9ffb7fbf69322f9d
l-gorman <lgorman@turing.ac.uk>
2022-01-19 10:49:09 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Button from 'react-bootstrap/Button';
+import scenarios from './components/scenarios/scenarios';
+
+import { useState, useEffect } from 'react';
+
+
+
+function HandleButtonPress(props) {
+  if (props.buttonText === "Start") {
+    props.setButtonText("Stop")
+    props.setButtonColor("#FF4136")
+    props.setSelecting(true)
+  }
+  if (props.buttonText === "Stop") {
+    props.setButtonText("Start")
+    props.setButtonColor("#0557FF")
+    props.setSelecting(false)
+  }
+}
+
+function Picker(props) {
+
+  const [selecting, setSelecting] = useState(false)
+  const [randomText, setRandomText] = useState("?")
+
+  const [buttonColor, setButtonColor] = useState("#0557FF")
+  const [buttonText, setButtonText] = useState("Start")
+
+  const [index, setIndex] = useState(0)
+
+  const intervalDuration = 100
+
+  const buttonStyle = {
+    "border": 0,
+    "background-color": buttonColor,
+    "width": "200px",
+    "color": "#D8D8D8",
+    "border-radius": "30px",
+    "font-size": "30px"
+  }
+
+  // Running the timer
+  useEffect(() => {
+    let interval = null
+    if (selecting) {
+      interval = setInterval(() => {
+        if (index < scenarios[props.data.type].options.length - 1) {
+          let newIndex = index + 1
+          setIndex(newIndex)
+          setRandomText(scenarios[props.data.type].options[newIndex])
+        }
+        if (index == scenarios[props.data.type].options.length - 1) {
+          setIndex(0)
+          setRandomText(scenarios[props.data.type].options[0])
+        }
+      }, intervalDuration);
+    } else if (!selecting) {
+      clearInterval(interval)
+    }
+
+
+
+    //Clear up
+    return () => {
+      clearInterval(interval)
+    }
+  }, [selecting, index])
+
+
+
+  return (
+    <div className='sub-page-div'>
+      <div className='sub-page-text'>
+        <h2>{scenarios[props.data.type].label}</h2>
+      </div>
+      <div className='sub-page-text'>
+        <h2>{randomText}</h2>
+      </div>
+      <div className='sub-page-button'>
+        <Button style={buttonStyle}
+          onClick={(event) => {
+            HandleButtonPress({
+              setButtonColor: setButtonColor,
+              buttonText: buttonText,
+              setButtonText: setButtonText,
+              pressed: true,
+              setSelecting: setSelecting,
+              selecting: selecting,
+              index: index,
+              setIndex: setIndex
+            })
+          }}>{buttonText}</Button>
+      </div>
+    </div >
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-div'>
+      <h1> Sample Data Hazards Scenario Selector</h1>
+      <div className='break'></div>
+      <Picker data={{ "type": "scenario" }} />
+      <Picker data={{ "type": "outcome" }} />
     </div>
   );
 }
---
+++ src/components/scenarios/scenarios.js
@@ -0,0 +1,22 @@
+
+const scenarios = {
+    scenario: {
+        label: "What's the worst thing that could happen if...",
+        options: [
+            "you applied algorithm x in a healthcare setting?",
+            "you made this data public?",
+            "you presented your presented your data in this way?"
+
+        ]
+    },
+    outcome: {
+        label: "and...",
+        options: [
+            "it went well",
+            "it went badly",
+            "some other thing happened"
+        ]
+    }
+}
+
+export default scenarios
